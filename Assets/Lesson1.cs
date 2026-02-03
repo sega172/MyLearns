fac8d78138adc4b565efb95aaac8076cf8f28685
_sega <seregaabond@gmail.com>
2026-02-03 14:35:07 +0300

    Initial commit

---
+++ Assets/Lesson1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Lesson1 : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("пробел");
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            print("ЛКМ");
+        }
+
+        float h = Input.GetAxis("Horizontal");
+    }  
+}
