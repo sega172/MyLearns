4641c7e8a49d2e9563beca9e3c171cb2786f4d4f
_sega <seregaabond@gmail.com>
2026-02-03 15:28:40 +0300

    Пример создания editor окна через скрипт

---
+++ Assets/Editor/QuickTool.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class QuickTool : EditorWindow
+{
+    [MenuItem("QuickTool/Open _%#T")]
+    public static void ShowWindow()
+    {
+        //открывает окно. Если открыто, ставит фокус на него.
+        var window = GetWindow<QuickTool>();
+
+        //добавить заголовок
+        window.titleContent = new GUIContent("QuickTool");
+
+        //Задать минимальный размер
+        window.minSize = new Vector2(280, 50);
+    }
+
+
+    private void CreateGUI()
+    {
+        //ссылка на корневой элемент окна
+        var root = rootVisualElement;
+
+        //создание кнопки и инициализация поля текст
+        var myButton = new Button() { text = "My Button" };
+
+        //раздать стиля
+        myButton.style.width = 160;
+        myButton.style.height = 30;
+
+        //добавить под корень
+        root.Add(myButton);
+    }
+
+}
