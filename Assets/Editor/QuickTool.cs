80ea41dc3b833edc73bb0ad000de15c7ac1bf894
_sega <seregaabond@gmail.com>
2026-02-03 16:09:40 +0300

    Туториал завершен
    Сделан EditorTool для создания примитивов на сцене
    https://learn.unity.com/tutorial/ui-toolkit-first-steps?version=2021.1

--- Assets/Editor/QuickTool.cs
+++ Assets/Editor/QuickTool.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -20,6 +21,8 @@ public class QuickTool : EditorWindow
 
     private void CreateGUI()
     {
+        /*пример добавления ui элмента через скрипт
+        
         //ссылка на корневой элемент окна
         var root = rootVisualElement;
 
@@ -32,6 +35,54 @@ public class QuickTool : EditorWindow
 
         //добавить под корень
         root.Add(myButton);
+        */
+
+        var root = rootVisualElement;
+
+        //подтягиваем USS
+        root.styleSheets.Add(Resources.Load<StyleSheet>("QuickTool_Style"));
+
+        //подгрузить дерево из файла
+        var quickToolVisualTree = Resources.Load<VisualTreeAsset>("QuickTool_Main");
+        //клонировать дерево в корень
+        quickToolVisualTree.CloneTree(root);
+
+        //получаем все кнопки из дерева по классу quicktool-button
+        var toolButtons = root.Query(className: "quicktool-button");
+        //пропускаем каждую кнопку через метод SetupButton
+        toolButtons.ForEach(SetupButton);
+    }
+
+    private void SetupButton(VisualElement button)
+    {
+        //ищем под кнопкой элемент с нужным классом? да. И извлкаем его
+        var buttonIcon = button.Q(className: "quicktool-button-icon");
+
+        //определем имя иконки
+        var iconPath = "Icons/" + button.parent.name + "_icon";
+        //подгружаем иконку по пути
+        var iconAsset = Resources.Load<Texture2D>(iconPath);
+
+        //ставим иконку как bg кнопки
+        buttonIcon.style.backgroundImage = iconAsset;
+
+        //callback: спавним примитив по нажатию
+        button.RegisterCallback<PointerUpEvent, string>(CreateObject, button.parent.name);
+
+        button.tooltip = button.parent.name;
+    }
+
+    //собственно вызываемый кнопкой метод
+    private void CreateObject(PointerUpEvent _, string primitiveTypeName)
+    {
+        //определяем какой примитив нам нужен по имени из параметра
+        var pt = (PrimitiveType)Enum.Parse(enumType: typeof(PrimitiveType),
+                                           value: primitiveTypeName,
+                                           ignoreCase: true);
+
+        //фабрика для создания из эдитора? что то типа аналога Instantiate?
+        var go = ObjectFactory.CreatePrimitive(pt);
+        go.transform.position = Vector3.zero;
     }
 
 }
