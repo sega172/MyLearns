d432788ddcb3c7caaee0997a8b6d2ebd69d62d08
_sega <seregaabond@gmail.com>
2026-02-04 10:55:48 +0300

    Добавлен пример UI Toolkit использования из документации

---
+++ Assets/Editor/SimpleCustomEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SimpleCustomEditor : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("Window/UI Toolkit/SimpleCustomEditor")]
+    public static void ShowExample()
+    {
+        SimpleCustomEditor wnd = GetWindow<SimpleCustomEditor>();
+        wnd.titleContent = new GUIContent("SimpleCustomEditor");
+    }
+
+    private int m_ClickCount = 0;
+    private const string m_ButtonPrefix = "button";
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Элементы управления созданные с использованием C# кода");
+        root.Add(label);
+
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+
+        SetupButtonHandler();
+    }
+
+    private void SetupButtonHandler()
+    {
+        VisualElement root = rootVisualElement;
+
+        var buttons = root.Query<Button>();
+        buttons.ForEach(RegisterHandler);
+    }
+
+    private void RegisterHandler(Button button)
+    {
+        button.RegisterCallback<ClickEvent>(PrintClickMessage);
+    }
+
+    private void PrintClickMessage(ClickEvent evt)
+    {
+        VisualElement root = rootVisualElement;
+        ++m_ClickCount;
+
+        Button button = evt.currentTarget as Button;
+        string buttonNumber = button.name.Substring(m_ButtonPrefix.Length);
+        string toggleName = "toggle" + buttonNumber;
+
+        Toggle toggle = root.Q<Toggle>(toggleName);
+
+        Debug.Log("Button was clicked" + (toggle.value ? " Count: " + m_ClickCount : ""));
+
+    }
+}
