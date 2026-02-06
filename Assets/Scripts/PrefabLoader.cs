0457169dd3571da89bcb5d392250e9518846ecbc
_sega <seregaabond@gmail.com>
2026-02-06 14:30:33 +0300

    Подгрузка префаба addressables + async

---
+++ Assets/Scripts/PrefabLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class PrefabLoader : MonoBehaviour
+{
+    [SerializeField] AssetReference prefabRef;
+
+
+    public void LoadPrefabPlease() => LoadPrefab();
+
+    private async void LoadPrefab()
+    {
+        AsyncOperationHandle<GameObject> handle = prefabRef.LoadAssetAsync<GameObject>();
+        await handle.Task;
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            GameObject prefab = handle.Result;
+            Instantiate(prefab);
+            Addressables.Release(handle);
+        }
+    }
+}
