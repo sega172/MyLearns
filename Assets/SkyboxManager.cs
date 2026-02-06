4abded8a7948938263d8a5a75dbc67cefa39a312
_sega <seregaabond@gmail.com>
2026-02-06 13:46:34 +0300

    Подгрузка скайбокса через Addressables

---
+++ Assets/SkyboxManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class SkyboxManager : MonoBehaviour
+{
+    [SerializeField] List<AssetReference> SkyboxMaterials;
+
+    private AsyncOperationHandle<Material> operationHandle;
+
+
+    public void SetSkybox(int index)
+    {
+        StartCoroutine(SetSkyboxInternal(index));
+    }
+
+    private IEnumerator SetSkyboxInternal(int index)
+    {
+        if (operationHandle.IsValid())
+        {
+            Addressables.Release(operationHandle);
+        }
+
+        AssetReference skyboxMaterialReference = SkyboxMaterials[index];
+        operationHandle = skyboxMaterialReference.LoadAssetAsync<Material>();
+
+        yield return operationHandle;
+        RenderSettings.skybox = operationHandle.Result;
+    }   
+}
